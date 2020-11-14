fb6134163c84e94cca6d07a47e4591228e5aa851
Penguin-God <gkswh4860@naver.com>
2020-11-14 13:33:47 +0900

    파일 정리

--- 버린코드.js
+++
@@ -1,9 +0,0 @@
-var 번역할언어 = document.getElementById("번역할언어").value.split(" ")
-    for(var i = 0; i < 번역할언어.length; i++){
-        var 바꿀언어 = " " + 번역할언어[i]
-        for(var key in 제주어사전){
-            if(바꿀언어.indexOf(key) != -1)
-                번역할언어[i] = 제주어사전[key]
-        }
-        번역된언어 += 번역할언어[i] + " "
-    }
